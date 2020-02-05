12a8f24df91da7616b461267a1bca9d0ca7180fe
Remco Kruit <remco.kruit@alten.nl>
2020-02-05 17:03:22 +0100

    Commit viewmodels and repository and service interfaces

---
+++ CleanArchitecure/CleanAchitecture.Domain/Interfaces/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using CleanAchitecture.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanAchitecture.Domain.Interfaces
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<CourseEntity> GetCourses();
+    }
+}
---
+++ CleanArchitecure/CleanArchitecture.Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CleanAchitecture.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Application.ViewModels
+{
+    public class CourseViewModel
+    {
+        public IEnumerable<CourseEntity> Courses { get; set; }
+    }
+}
