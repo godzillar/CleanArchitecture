4a3bebf87b638af488b80c405f9a5b667c07d754
Remco Kruit <remco.kruit@alten.nl>
2020-02-06 13:44:21 +0100

    Added implementation of repository and service

---
+++ CleanArchitecure/CleanArchitecture.Application/Services/CourseService.cs
@@ -0,0 +1,26 @@
+﻿using CleanAchitecture.Domain.Interfaces;
+using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly ICourseRepository courseRepository;
+
+        public CourseService(ICourseRepository courseRepository) {
+            this.courseRepository = courseRepository;
+        }
+
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel
+            {
+                Courses = courseRepository.GetCourses()
+            };
+        }
+    }
+}
---
+++ CleanArchitecure/CleanArchitecture.Infra.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanAchitecture.Domain.Interfaces;
+using CleanAchitecture.Domain.Models;
+using CleanArchitecture.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Infra.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDBContext context;
+
+        public CourseRepository(UniversityDBContext context) {
+            this.context = context;
+        }
+
+        public IEnumerable<CourseEntity> GetCourses()
+        {
+            return context.Courses;
+        }
+    }
+}
