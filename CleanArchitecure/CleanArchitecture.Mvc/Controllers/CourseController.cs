55da16408e041da6904fd434f63528a56be401c2
Remco Kruit <remco.kruit@alten.nl>
2020-02-06 14:43:50 +0100

    Update presentation layer with courses view.

---
+++ CleanArchitecure/CleanArchitecture.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanArchitecture.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            this.courseService = courseService;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var viewModel = courseService.GetCourses();
+
+            return View(viewModel);
+        }
+    }
+}
