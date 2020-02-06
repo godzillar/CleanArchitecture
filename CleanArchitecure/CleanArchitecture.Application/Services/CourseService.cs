20aef2c256a5032ece1cabd7457c2a6f62156541
Remco Kruit <remco.kruit@alten.nl>
2020-02-06 15:26:55 +0100

    Add create course method.

--- CleanArchitecure/CleanArchitecture.Application/Services/CourseService.cs
+++ CleanArchitecure/CleanArchitecture.Application/Services/CourseService.cs
@@ -1,6 +1,8 @@
-﻿using CleanAchitecture.Domain.Interfaces;
+﻿using CleanAchitecture.Domain.Commands;
+using CleanAchitecture.Domain.Interfaces;
 using CleanArchitecture.Application.Interfaces;
 using CleanArchitecture.Application.ViewModels;
+using CleanArchitecture.Domain.Core.Bus;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,9 +12,18 @@ namespace CleanArchitecture.Application.Services
     public class CourseService : ICourseService
     {
         private readonly ICourseRepository courseRepository;
+        private readonly IMediatorHandler bus;
 
-        public CourseService(ICourseRepository courseRepository) {
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus) {
             this.courseRepository = courseRepository;
+            this.bus = bus;
+        }
+
+        public void Create(CourseViewModel courseViewModel)
+        {
+            var command = new CreateCourseCommand(courseViewModel.Name, courseViewModel.Description, courseViewModel.ImageUrl);
+            
+            bus.SendCommand(command);
         }
 
         public CourseViewModel GetCourses()
--- CleanArchitecure/CleanArchitecture.Application/ViewModels/CourseViewModel.cs
+++ CleanArchitecure/CleanArchitecture.Application/ViewModels/CourseViewModel.cs
@@ -7,6 +7,12 @@ namespace CleanArchitecture.Application.ViewModels
 {
     public class CourseViewModel
     {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl {get; set;}
+
         public IEnumerable<CourseEntity> Courses { get; set; }
     }
 }
