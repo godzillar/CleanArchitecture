dee59999dac4b940a792119e8412ba4f7ee8029f
Remco Kruit <remco.kruit@alten.nl>
2020-02-06 15:04:41 +0100

    Adding Domain.Core and implementation of InMemoryBus class.

---
+++ CleanArchitecure/CleanArchitecture.Domain.Core/Events/Message.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Domain.Core.Events
+{
+    public abstract class Message: IRequest<bool>
+    {
+        protected Message()
+        {
+            MessageType = GetType().Name;
+        }
+
+        public string MessageType { get; protected set; }
+    }
+}
--- CleanArchitecure/CleanArchitecture.Infra.Bus/InMemoryBus.cs
+++ CleanArchitecure/CleanArchitecture.Infra.Bus/InMemoryBus.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using CleanArchitecture.Domain.Core.Bus;
+using CleanArchitecture.Domain.Core.Commands;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CleanArchitecture.Infra.Bus
 {
-    public class InMemoryBus
+    public class InMemoryBus: IMediatorHandler
     {
+        private readonly IMediator mediator;
+
+        public InMemoryBus(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public Task SendCommand<T>(T command) where T: Command
+        {
+            return mediator.Send(command);
+        }
     }
 }
