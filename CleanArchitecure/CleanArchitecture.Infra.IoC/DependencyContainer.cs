27188e271c3cdac56acc4060296d4a05936313d6
Remco Kruit <remco.kruit@alten.nl>
2020-02-06 13:55:22 +0100

    Added dependency container class and called registration of services.

---
+++ CleanArchitecure/CleanArchitecture.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using CleanAchitecture.Domain.Interfaces;
+using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.Services;
+using CleanArchitecture.Infra.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Infra.IoC
+{
+    public static class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<ICourseService, CourseService>();
+            services.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
